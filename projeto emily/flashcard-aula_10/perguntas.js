d4ef8baa526ef44b047847f8801153eba8d0d3b9
Jao8-o <joaovgromero@gmail.com>
2024-10-14 17:42:05 -0300

    Add files via upload

---
+++ projeto emily/flashcard-aula_10/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matematica',
+    'Qunato é 2+2?',
+    '4'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual o nome da capital do Brasil?',
+    'A capital do Brasil é brasilia'
+)
+
+criaCartao(
+    'Harry Potter',
+    'Qual é a casa do Harry?',
+    'Grifinoria'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tigre em Inglês?',
+    'tigre em ingles é tiger)'
+)
